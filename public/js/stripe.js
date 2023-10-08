20486c1b60db8c6fea7e4ec884083914c5171db2
Huzefa Badshah <501huzefa@gmail.com>
2023-10-08 20:57:47 +0530

    update latest credientials for mailtrap and stripe

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 import { showALert } from './alert';
 
-export const bookTour = async function(tourId, target) {
+export const bookTour = async function (tourId, target) {
   try {
-    const stripe = Stripe('pk_test_QhZ3nPfp4ihFHGNBfUDJteGN0049TzeF2c');
+    const stripe = Stripe('pk_test_51NhMyTSEtyLZFqLyNnNGIkel1qQ8t8KlaQuoH5ixZ197UgdlEX86RnjuzFaBENWFgtPCAsVLIa6pC9RXsTdO9jZm00n4y4hLbN');
 
     // Get cehckout session from API
-    const {data: {session}} = await axios({
+    const { data: { session } } = await axios({
       method: 'GET',
       url: `/api/v1/bookings/checkout-session/${tourId}` // relative url
     });
-   
+
     target.textContent = 'Done';
 
     // Create checkout form + charge credit card
     await stripe.redirectToCheckout({
-        sessionId: session.id
+      sessionId: session.id
     });
   } catch (err) {
     console.log('error', err);
